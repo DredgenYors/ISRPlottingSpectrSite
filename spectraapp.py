6587ef7a3bd8fbc3739c5aa78b61a2dceef1a554
Brandon Lee <99155935+DredgenYors@users.noreply.github.com>
2025-02-12 12:55:32 -0500

    Added ISR Site Navigation
    Adjusted navigation routes to now serve the main isr site as the homepage

--- spectraapp.py
+++ spectraapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, request, jsonify, send_file, redirect, url_for
 import matplotlib.pyplot as plt
 import numpy as np
 import io
@@ -19,66 +19,96 @@ from spectratestingmodified_chirag_edit import (
 
 app = Flask(__name__)
 
+# Default parameter values
+user_values = {
+    "nu_i": 1.0e-7,
+    "nu_e": 1.0e-7,
+    "ni": 2.0e11,
+    "ne": 2.0e11,
+    "mi": 2.65686e-26,
+    "me": 9.11e-31,
+    "B": 3.6e-5,
+    "theta": 60,
+    "Te": 500,
+    "epsilon_0": 8.854187817e-12,
+    "kB": 1.380649e-23,
+    "e": 1.602e-19,
+    "n_terms": 2000
+}
+
 @app.route('/')
 def home():
+    """ Render the main landing page: index.html """
+    return render_template('index.html', user_values=user_values)
+
+@app.route('/spectrasite')
+def spectrasite():
+    """ Redirect to the spectrasite page when 'Simulation' is clicked """
     return render_template('spectrasite.html')
 
 @app.route('/plot_page')
 def plot_page():
+    """ Serve the existing plot page """
     return render_template('plot_page.html')
+
+@app.route('/update_values', methods=['POST'])
+def update_values():
+    """ Update values dynamically from user input """
+    global user_values
+    data = request.json  # Get user input from AJAX request
+    for key in data:
+        user_values[key] = float(data[key])  # Convert all inputs to float
+    return jsonify({"message": "Values updated"}), 200
 
 @app.route('/plot')
 def plot():
-    # Define constants
-    nu_i = .0000001  # Ion collision frequency in Hz
-    nu_e = 0.0000001  # Electron collision frequency in Hz
-    ni = ne = 2e11  # Ion and electron densities in m^-3
-    mi = 2.65686e-26  # Ion mass (atomic oxygen) in kg
-    m_e = 9.11e-31  # Electron mass [kg]
-    B = 3.6e-5  # Magnetic field strength in Tesla
-    theta = 60  # Scattering angle in degrees
-    Te_values = [500, 1500, 2500, 3500]  # Electron temperatures in Kelvin
-    epsilon_0 = 8.854187817e-12  # Vacuum permittivity [F/m]
-    kB = 1.380649e-23  # Boltzmann constant [J/K]
-    e = 1.602e-19  # Elementary charge [C]
-    n_terms = 2000
+    """ Generate the plot using the latest user input values """
+    
+    # Load user-defined parameters
+    nu_i = user_values["nu_i"]
+    nu_e = user_values["nu_e"]
+    ni = user_values["ni"]
+    ne = user_values["ne"]
+    mi = user_values["mi"]
+    me = user_values["me"]
+    B = user_values["B"]
+    theta = user_values["theta"]
+    Te = user_values["Te"]
+    epsilon_0 = user_values["epsilon_0"]
+    kB = user_values["kB"]
+    e = user_values["e"]
+    n_terms = int(user_values["n_terms"])  # Convert to integer
 
-    # Calculate wavenumber components
+    # Compute necessary values
     k_total, k_parallel, k_perpendicular = calculate_wavenumber_components(0.69719, theta)
+    vth_i = calculate_thermal_velocity(kB, Te, mi)
+    vth_e = calculate_thermal_velocity(kB, Te, me)
+    Oc_i = calculate_cyclotron_frequency(1, e, B, mi)
+    Oc_e = calculate_cyclotron_frequency(1, e, B, me)
+    rho_i = calculate_average_gyroradius(vth_i, Oc_i)
+    rho_e = calculate_average_gyroradius(vth_e, Oc_e)
+    lambda_De = calculate_debye_length(Te, ne, epsilon_0, kB, e)
+    alpha_e = calculate_alpha(k_total, lambda_De)
+    c = calculate_sound_speed(kB, Te, Te, mi)
+    omega_values = calculate_omega_values(k_total, c)
 
-    spectra_list = []
-    for T in Te_values:
-        Te = Ti = T
-        vth_i = calculate_thermal_velocity(kB, Ti, mi)
-        vth_e = calculate_thermal_velocity(kB, Te, m_e)
-        Oc_i = calculate_cyclotron_frequency(1, e, B, mi)
-        Oc_e = calculate_cyclotron_frequency(1, e, B, m_e)
-        rho_i = calculate_average_gyroradius(vth_i, Oc_i)
-        rho_e = calculate_average_gyroradius(vth_e, Oc_e)
-        lambda_De = calculate_debye_length(Te, ne, epsilon_0, kB, e)
-        lambda_Di = calculate_debye_length(Te, ni, epsilon_0, kB, e)
-        alpha_e = calculate_alpha(k_total, lambda_De)
-        alpha_i = calculate_alpha(k_total, lambda_Di)
-        c = calculate_sound_speed(kB, Te, Ti, mi)
-        omega_values = calculate_omega_values(k_total, c)
-        U_i = calculate_collisional_term(nu_i, k_parallel, vth_i, k_perpendicular, rho_i, n_terms, omega_values, Oc_i)
-        U_e = calculate_collisional_term(nu_e, k_parallel, vth_e, k_perpendicular, rho_e, n_terms, omega_values, Oc_e)
-        M_i = calculate_modified_distribution(omega_values, k_parallel, k_perpendicular, vth_i, n_terms, rho_i, Oc_i, nu_i, U_i)
-        M_e = calculate_modified_distribution(omega_values, k_parallel, k_perpendicular, vth_e, n_terms, rho_e, Oc_e, nu_e, U_e)
-        chi_i = calculate_electric_susceptibility(omega_values, k_parallel, k_perpendicular, vth_i, n_terms, rho_i, Oc_i, nu_i, alpha_i, U_i, Ti, Ti)
-        chi_e = calculate_electric_susceptibility(omega_values, k_parallel, k_perpendicular, vth_e, n_terms, rho_e, Oc_e, nu_e, alpha_e, U_e, Te, Te)
-        spectra = calcSpectra(M_i, M_e, chi_i, chi_e)
-        spectra_list.append(spectra)
+    # Compute modified distributions and susceptibilities
+    U_i = calculate_collisional_term(nu_i, k_parallel, vth_i, k_perpendicular, rho_i, n_terms, omega_values, Oc_i)
+    U_e = calculate_collisional_term(nu_e, k_parallel, vth_e, k_perpendicular, rho_e, n_terms, omega_values, Oc_e)
+    M_i = calculate_modified_distribution(omega_values, k_parallel, k_perpendicular, vth_i, n_terms, rho_i, Oc_i, nu_i, U_i)
+    M_e = calculate_modified_distribution(omega_values, k_parallel, k_perpendicular, vth_e, n_terms, rho_e, Oc_e, nu_e, U_e)
+    chi_i = calculate_electric_susceptibility(omega_values, k_parallel, k_perpendicular, vth_i, n_terms, rho_i, Oc_i, nu_i, alpha_e, U_i, Te, Te)
+    chi_e = calculate_electric_susceptibility(omega_values, k_parallel, k_perpendicular, vth_e, n_terms, rho_e, Oc_e, nu_e, alpha_e, U_e, Te, Te)
 
-    # Plot spectra
+    # Calculate spectra using the updated parameters
+    spectra = calcSpectra(M_i, M_e, chi_i, chi_e)
+
+    # Plot the updated spectra
     fig, ax = plt.subplots(figsize=(10, 6))
-    colors = [(0, 0.4470, 0.7410), (0.8500, 0.3250, 0.0980), (0.9290, 0.6940, 0.1250), (0.4940, 0.1840, 0.5560)]
-    labels = [f'Te = {T} K' for T in Te_values]
-    for i, spectra in enumerate(spectra_list):
-        ax.plot(omega_values / (2 * np.pi * 1e6), spectra, color=colors[i], label=labels[i])
+    ax.plot(omega_values / (2 * np.pi * 1e6), spectra, color="b", label=f'Te = {Te} K')
     ax.set_xlabel('Frequency (MHz)')
     ax.set_ylabel('Spectra')
-    ax.set_title('Full Spectra for Different Electron Temperatures (Te)')
+    ax.set_title('Updated Spectra Based on User Input')
     ax.legend()
     ax.grid(True)
     ax.set_yscale('log')
@@ -86,7 +116,7 @@ def plot():
     ax.set_xlim(-6, 6)
     plt.tight_layout()
 
-    # Save plot to a BytesIO object
+    # Save plot to a BytesIO object and send to user
     img = io.BytesIO()
     plt.savefig(img, format='png')
     img.seek(0)
@@ -95,3 +125,4 @@ def plot():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
